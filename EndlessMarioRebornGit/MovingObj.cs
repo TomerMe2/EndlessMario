18513172998774bf8ad5a7dafae7e8e053a2bd21
TomerMe2 <tomerlao@post.bgu.ac.il>
2018-08-10 22:34:16 +0300

    gameObject now can Jump and Fall

--- EndlessMarioRebornGit/MovingObj.cs
+++ EndlessMarioRebornGit/MovingObj.cs
@@ -15,30 +15,93 @@ namespace EndlessMarioRebornGit
     }
     abstract class MovingObj : GameObject
     {
-        private List<Texture2D> textures;
+        private List<Texture2D> texturesFacingRight;
+        private List<Texture2D> texturesFacingLeft;
         protected bool isWalking;
         protected bool isFlipped;
         protected float accelerationX;
         protected float speedX;
         protected float accelerationY;
         protected float speedY;
-
-        public MovingObj(List<Texture2D> textures, Vector2 startLoc, float scale, bool isCollideAble)
-            : base(startLoc, textures.ElementAt(0), scale, isCollideAble)
+        private float maxSpeed;
+        private float jumpingPower;
+        private float walkingPower;
+        
+        public MovingObj(List<Texture2D> texturesFacingRight, List<Texture2D> texturesFacingLeft, Vector2 startLoc, float scale, bool isCollideAble, float walkingPower, 
+            float jumpingPower, float maxSpeed) : base(startLoc, texturesFacingRight.ElementAt(0), scale, isCollideAble)
         {
-            this.textures = textures;
+            //Texture Laws:
+            //First will come the statnding texture
+            //Then, the walking textures
+            //Then, the jumping texture
+            this.texturesFacingRight = texturesFacingRight;
+            this.texturesFacingLeft = texturesFacingLeft;
             isWalking = false;
             isFlipped = false;
+            accelerationX = 0;
+            accelerationY = 0;
+            speedX = 0;
+            speedY = 0;
+            this.maxSpeed = maxSpeed;
+            this.jumpingPower = jumpingPower;
+            this.walkingPower = walkingPower;
         }
         
+        /// <summary>
+        /// Initiates jump 
+        /// </summary>
         public virtual void Jump()
         {
+            if (speedY == 0)   //if it's not jumping alraedy
+            {
+                accelerationY = Physics.GRAVITY;
+                speedY = - jumpingPower + accelerationY;
+                //Updating the textures
+                if (isFlipped)
+                {
+                    currentTexture = texturesFacingLeft.ElementAt(texturesFacingLeft.Count - 1); 
+                }
+                else
+                {
+                    currentTexture = texturesFacingRight.ElementAt(texturesFacingLeft.Count - 1);
+                }
+            }
+        }
 
+        /// <summary>
+        /// Initiates fall
+        /// </summary>
+        public void Fall()
+        {
+            if (speedY == 0)   //if it's not falling already
+            {
+                accelerationY = Physics.GRAVITY;
+                speedY = accelerationY;
+            }
         }
 
         public virtual void Walk(Direction dir, float speed)
         {
 
+        }
+
+        /// <summary>
+        /// Flip the object
+        /// Keeps the same texture, but makes it flipped
+        /// </summary>
+        protected void FlipTexture()
+        {
+            if (isFlipped) //Facing left
+            {
+                int currIndex = texturesFacingLeft.FindIndex(new Predicate<Texture2D>(currentTexture.Equals)); //Finds the index of the current texture
+                currentTexture = texturesFacingRight.ElementAt(currIndex); //make the flip!
+            }
+            else //Facing right
+            {
+                int currIndex = texturesFacingRight.FindIndex(new Predicate<Texture2D>(currentTexture.Equals)); //Finds the index of the current texture
+                currentTexture = texturesFacingLeft.ElementAt(currIndex); //make the flip!
+            }
+            isFlipped = !isFlipped; //flip the flag
         }
     }
 }
---
+++ EndlessMarioRebornGit/Physics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndlessMarioRebornGit
+{
+    static class Physics
+    {
+        public const float GRAVITY = 10;
+    }
+}
